408d842c0308b7194f5631e67ab164e945a106a5
bmajczak <bmajczak10@gmail.com>
2023-10-22 18:58:56 +0200

    now both apps have access to database

--- db_setup.sh
+++ db_setup.sh
@@ -12,8 +12,10 @@ sudo mysqladmin -u root password "$DATABASE_PASS"
 sudo mysql -u root -p"$DATABASE_PASS" -e "create database users"
 sudo mysql -u root -p"$DATABASE_PASS" -e 'use users; create table `user` (`id` int(11) not null auto_increment, `name` varchar(255) default null, `lastName` varchar(255) default null, primary key(`id`));'
 sudo mysql -u root -p"$DATABASE_PASS" -e 'use users; insert into user (name, lastName) values ("imie1","nazwisko1"),("imie2","nazwisko2");'
-sudo mysql -u root -p"$DATABASE_PASS" -e 'CREATE OR REPLACE USER app@192.168.56.12 IDENTIFIED BY "app123";'
+sudo mysql -u root -p"$DATABASE_PASS" -e 'CREATE USER app@192.168.56.12 IDENTIFIED BY "app123";'
+sudo mysql -u root -p"$DATABASE_PASS" -e 'CREATE USER app@192.168.56.13 IDENTIFIED BY "app123";'
 sudo mysql -u root -p"$DATABASE_PASS" -e 'GRANT select ON users.user TO app@192.168.56.12;'
+sudo mysql -u root -p"$DATABASE_PASS" -e 'GRANT select ON users.user TO app@192.168.56.13;'
 
 
 
